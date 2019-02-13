f9ba17f4f163ea1ded588aa8dd5b60d8486e6c8e
Jason Mei <jason.mei403@gmail.com>
2019-02-13 00:58:37 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -23,6 +23,7 @@ class LandmarksController < ApplicationController
   end
 
   patch '/landmarks/:id' do
+    binding.pry
     @landmark = Landmark.find_by_id[params[:id]]
     @landmark.update(params[:landmark])
     redirect "/landmarks/#{@landmark.id}"
